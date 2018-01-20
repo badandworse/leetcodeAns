42eeb1efe85a1070950ece06db300bb57ccb264e
bad <zilaixv@gmail.com>
2018-01-20 13:37:22 +0800

    用二分查找完成第33题

--- answers/33SearchInRotatedSortedArray.py
+++ answers/33SearchInRotatedSortedArray.py
@@ -1,3 +1,4 @@
+#%%
 class Solution:
     def search(self, nums, target):
         """
@@ -12,3 +13,44 @@ class Solution:
                 return i
             i+=1
         return -1
+
+    if len(nums)==0:
+            return -1
+        begin=0
+        end=len(nums)-1
+        Nmin=0
+        while begin<=end:
+            mid=begin+int((end-begin)/2)
+            if nums[mid]<nums[Nmin]:
+                Nmin=mid
+                end=mid-1
+            else:
+                begin=mid+1
+        
+        
+        if nums[Nmin]==target:
+            return Nmin
+        i=0
+        end=Nmin-1 if Nmin>0 else len(nums)-1
+        tIndex=self.findT(nums,i,end,target)
+        if tIndex>-1:
+            return tIndex
+        i=Nmin+1
+        end=len(nums)-1 if Nmin>0 else-1
+        return self.findT(nums,i,end,target)
+    
+    def findT(self,nums,i,end,target):
+        while i<=end:
+            mid=i+int((end-i)/2)
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                end=mid-1
+            else:
+                i=mid+1
+        
+        return -1
+#%%
+msss=Solution()
+aList=[3,1]
+msss.BinarySearch(aList,1)
