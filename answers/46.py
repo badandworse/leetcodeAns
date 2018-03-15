09c5807ffb92b2981d6a76f08567e90d99255f87
bad <zilaixv@gmail.com>
2018-03-15 22:02:03 +0800

    用python自带的itertools.permutations完成46

--- answers/46.py
+++ answers/46.py
@@ -1,3 +1,4 @@
+#%%
 class Solution:
     def permute(self, nums):
         """
@@ -17,3 +18,26 @@ class Solution:
             for i in range(mlen+1):
                 results.append(results[0][:i]+[x]+results[0][i:])
             del results[0]
+
+class Solution1:
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        results=[[]]
+        for num in nums:
+            self.genRe(results,num)
+        return results
+    
+    def genRe(self,results,num):
+        length=len(results)
+        for i in range(length):
+            mList=results[i]
+            for j in range(len(mList)+1):
+                results.append(mList[:j]+[num]+mList[j+1:])
+        del results[:length]
+
+
+mm=Solution1()
+mm.permute([1,2,3])
