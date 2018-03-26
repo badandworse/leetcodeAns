eba8c5eabe359352b5504c76a3ae0b0b7f45663e
bad <zilaixv@gmail.com>
2018-03-26 12:03:38 +0800

    438 更好的答案。

--- answers/438.py
+++ answers/438.py
@@ -49,5 +49,31 @@ class Solution:
                 mm=tempDict[s[end-1]][0]+1
                 return mm,{}
 
-mm=Solution()
-mm.findAnagrams("abab","ab")
+
+'''
+better anwser
+'''
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        res = []
+        n, m = len(s), len(p)
+        if n < m: return res
+        phash, shash = [0]*123, [0]*123
+        for x in p:
+            phash[ord(x)] += 1
+        for x in s[:m-1]:
+            shash[ord(x)] += 1
+        for i in range(m-1, n):
+            shash[ord(s[i])] += 1
+            if i-m >= 0:
+                shash[ord(s[i-m])] -= 1
+            if shash == phash:
+                res.append(i - m + 1)
+        return res
+
+        
